2e8c7ebd0922dfbaf36e5875c3e3ac1f12a33a58
jorge candeias do nascimento <elntele@gmail.com>
2020-01-27 20:12:58 -0300

    adição da fução correta para os 8 melhores individuos e também modificação do metodo de continuar a partir de uma execução que parou(ainda bugado poque retor aos centoids originais)

--- src/br/cns/persistence/GmlDao.java
+++ src/br/cns/persistence/GmlDao.java
@@ -280,6 +280,155 @@ public class GmlDao implements IGmlDao {
 		}
 		return data;
 	}
+	
+	/**
+	 * metodo copiado do de loadGmlData exatamente igual, porem sem a 
+	 * parte que muda os ids autor jorge candeias
+	 * @param path
+	 * @param allowIslands
+	 * @return
+	 */
+	
+	
+	public GmlData loadGmlDataWithTheSamId(String path, boolean allowIslands) {
+		GmlData data = new GmlData();
+		int state = 0;
+
+		try {
+			FileReader fr = new FileReader(new File(path));
+			LineNumberReader lnr = new LineNumberReader(fr);
+			String line = lnr.readLine();
+			String lowerLine = null;
+			String[] parts = null;
+			GmlNode node = new GmlNode(-1);
+			GmlEdge edge = new GmlEdge();
+
+			data.setMaxLatitude(Double.NEGATIVE_INFINITY);
+			data.setMinLatitude(Double.POSITIVE_INFINITY);
+			data.setMinLongitude(Double.POSITIVE_INFINITY);
+			data.setMaxLongitude(Double.NEGATIVE_INFINITY);
+
+			while (line != null) {
+				lowerLine = line.trim().toLowerCase();
+				if (line.trim().equals("")) {
+					line = lnr.readLine();
+					continue;
+				}
+				if (lowerLine.startsWith("node")) {
+					state = 1;
+				} else if (lowerLine.startsWith("edge")) {
+					state = 2;
+				}
+				switch (state) {
+				case 0:
+					if (!(lowerLine.startsWith("graph") || lowerLine.startsWith("]"))) {
+						parts = split(line);
+						data.addInformation(parts[0], parts[1]);
+					}
+					break;
+				case 1:
+					if (lowerLine.startsWith("]") && node.getId() != -1) {
+						HashMap<String, String> infos = new HashMap<String, String>();
+						for (String i : node.getInformations().keySet()) {
+							infos.put(i, node.getInformations().get(i));
+						}
+						boolean added = data.addNode(node.getId(), node.getLabel(), node.getCountry(),
+								node.getLongitude(), node.getLatitude(), node.getInternal(), infos,
+								node.getPopulation());
+						if (added) {
+							if (node.getLongitude() > data.getMaxLongitude()) {
+								data.setMaxLongitude(node.getLongitude());
+							}
+							if (node.getLongitude() < data.getMinLongitude()) {
+								data.setMinLongitude(node.getLongitude());
+							}
+							if (node.getLatitude() > data.getMaxLatitude()) {
+								data.setMaxLatitude(node.getLatitude());
+							}
+							if (node.getLatitude() < data.getMinLatitude()) {
+								data.setMinLatitude(node.getLatitude());
+							}
+						}
+						node = new GmlNode(-1);
+					} else if (!lowerLine.startsWith("node") && !lowerLine.trim().equals("]")) {
+						parts = split(line);
+						if (parts[0].equals("id")) {
+							node.setId(Integer.parseInt(parts[1]));
+						} else if (parts[0].equals("label")) {
+							node.setLabel(parts[1]);
+						} else if (parts[0].equals("Country")) {
+							node.setCountry(parts[1]);
+						} else if (parts[0].equals("Longitude")) {
+							node.setLongitude(Double.parseDouble(parts[1]));
+						} else if (parts[0].equals("Internal")) {
+							node.setInternal(Integer.parseInt(parts[1]));
+						} else if (parts[0].equals("Latitude")) {
+							node.setLatitude(Double.parseDouble(parts[1]));
+						} else if (parts[0].equalsIgnoreCase("Population")) {
+							node.setPopulation(Long.parseLong(parts[1]));
+						} else if (!parts[0].trim().equals("")) {
+							node.getInformations().put(parts[0], parts[1]);
+						}
+					}
+					break;
+				case 2:
+					if (lowerLine.startsWith("]")) {
+						HashMap<String, String> infos = new HashMap<String, String>();
+						for (String i : edge.getInformations().keySet()) {
+							infos.put(i, edge.getInformations().get(i));
+						}
+						if (!data.getEdges().contains(edge) && edge.getSource().getId() != -1) {
+							data.addEdge(edge.getSource().getId(), edge.getTarget().getId(), edge.getLabel(), infos);
+						}
+						edge = new GmlEdge();
+					} else if (!lowerLine.startsWith("edge")) {
+						parts = split(line);
+						if (parts[0].equals("source")) {
+							edge.getSource().setId(Integer.parseInt(parts[1]));
+						} else if (parts[0].equals("target")) {
+							edge.getTarget().setId(Integer.parseInt(parts[1]));
+						} else if (parts[0].equals("LinkLabel")) {
+							edge.setLabel(parts[1]);
+						} else if (!parts[0].trim().equals("")) {
+							edge.getInformations().put(parts[0], parts[1]);
+						}
+					}
+					break;
+				}
+
+				line = lnr.readLine();
+			}
+
+			fr.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		data.setName((data.getInformations().get("GeoLocation") + " " + data.getInformations().get("Network")).trim());
+
+		boolean achou = false;
+		List<GmlNode> isolados = new Vector<>();
+		for (GmlNode node : data.getNodes()) {
+			achou = false;
+			for (GmlEdge edge : data.getEdges()) {
+				if (edge.getSource().equals(node) || edge.getTarget().equals(node)) {
+					achou = true;
+					break;
+				}
+			}
+			if (!achou) {
+				isolados.add(node);
+			}
+		}
+		if (!allowIslands) {
+			data.getNodes().removeAll(isolados);
+		}
+
+		return data;
+	}
+	
+	
+	
+	
 
 	public GmlData loadGmlDataFromContent(String content) {
 		GmlData data = new GmlData();
